0d91b26d1719cc0db6b607d1d621101ee5791a9c
jamesjmeyer210 <jamesjmeyer210@gmai.com>
2019-01-23 11:11:53 -0600

    Cleaned up Config.java and added welcome message

--- src/app/Config.java
+++ src/app/Config.java
@@ -5,24 +5,34 @@ import java.io.IOException;
 public class Config {
 
   private String pwd;
-  private String contactsPath;
+  //private String contactsPath;
+  private File contacts;
 
   public Config() {
-    String pwd = System.getProperty("user.dir"); // locates user directory
-    System.out.println(pwd);
-    String dataPath = pwd.replace("bin", "data"); //specifies contacts.txt location
-    System.out.println(dataPath);
-    File dataDir = new File (dataPath);
-    if (!dataDir.exists() ) {
+    // locates user directory
+    String pwd = System.getProperty("user.dir");
+
+    // create the path to the data directory
+    String dataPath = pwd.replace("bin", "data");
+    File dataDir = new File(dataPath);
+
+    if(!dataDir.exists()) {
+      // make the data directory, then the contacts file
       dataDir.mkdir();
-      contactsPath = new String (dataPath+"/contacts.txt");
-      File contacts = new File (contactsPath);
+      String contactsPath = new String(dataPath + "/contacts.txt");
+      this.contacts = new File(contactsPath);
+
+      // this will never throw an error so we can do a generic stack trace to
+      // satisfy the compiler
       try {
-        contacts.createNewFile();
+        this.contacts.createNewFile();
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
   }
 
+  public File getContacts(){
+    return this.contacts;
+  }
 }
--- src/app/ContactManager.java
+++ src/app/ContactManager.java
@@ -2,10 +2,32 @@ package app;
 
 public class ContactManager {
 
+  private static void printWelcome(){
+    String msg = "";
+    for(int i = 0; i < 64; i++){
+      msg += "=";
+    }
+    msg += "\n";
+    for(int i = 0; i < 20; i++){
+      msg += "=";
+    }
+    msg += " Contact Manager v0.0.1 ";
+    for(int i = 0; i < 20; i++){
+      msg += "=";
+    }
+    msg += "\n";
+    for(int i = 0; i < 64; i++){
+      msg += "=";
+    }
+    msg += "\n";
+    System.out.print(msg);
+  }
+
   public static void main(String[] args){
 
+    printWelcome();
+
     Switch<String,Runnable> options = new Switch<>();
-    System.out.println("Running...");
     Config config = new Config();
   }
 
