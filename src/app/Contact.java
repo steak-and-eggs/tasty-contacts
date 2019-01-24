f986d49cb3ae3558f47f1441541ac7ece77b46a9
jamesjmeyer210 <jamesjmeyer210@gmai.com>
2019-01-24 17:13:03 -0600

    Refractored to account for exceptions

--- src/app/Contact.java
+++ src/app/Contact.java
@@ -5,27 +5,12 @@ public class Contact {
   private String name;
   private int number;
 
-  public Contact(String nm, String num){
-    try {
-      this.name = nm;
-      this.number = Integer.parseInt(num);
-      //this.number = Integer.parseInt(this.sanitizeNumber(num));
-    } catch(NumberFormatException e){
-      System.out.println(num);
-      //System.out.println(this.sanitizeNumber(num));
-      e.printStackTrace();
-    }
+  public Contact(String nm, String num)
+    throws NumberFormatException
+  {
+    this.name = nm;
+    this.number = Integer.parseInt(num);
   }
-
-  // private String sanitizeNumber(String number){
-  //   String clean = "";
-  //   for(char ch : number.toCharArray()){
-  //     if(ch >= '0' && ch <= '9'){
-  //       clean += ch;
-  //     }
-  //   }
-  //   return clean;
-  // }
 
   public String toString(){
     return this.name + "," + this.number + ",";
--- src/app/ModAdd.java
+++ src/app/ModAdd.java
@@ -26,7 +26,14 @@ public class ModAdd extends Module {
       "\n"
     );
 
-    this.contactList.add(new Contact(name, sanitizeNumber(number)));
+    try {
+      this.contactList.add(new Contact(name, sanitizeNumber(number)));
+    } catch(NumberFormatException e){
+      System.out.format("Sorry, the phone number %s cannot be processed.\n",
+        number);
+      System.out.println("Please try only entering under the following format: (###)-###-####");
+    }
+
   }
 
   private String sanitizeNumber(String number){
